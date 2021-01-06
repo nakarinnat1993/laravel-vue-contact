d23c5fb2f46d64dccc478ff1fd4da53da59c4648
unknown <nakarinnat1993@gmail.com>
2021-01-06 21:42:37 +0700

    Setup project route

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app">
+        <h1>Hello</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"App"
+}
+</script>
+
+<style>
+
+</style>
